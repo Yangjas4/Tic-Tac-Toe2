16de825d15f7ab2ddb7b56854d2fa1796f3b2f80
Jason Yang <Jasonyang602@gmail.com>
2022-07-02 04:03:57 -0400

    react routing

---
+++ src/pages/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import HomeContent from '../components/HomeContent';
+
+export default function Homepage() {
+    return (
+        <div className="app">
+            <div className="margin">
+                <Navbar page="home"/>
+                <HomeContent />
+            </div>
+            <Footer />
+        </div>
+    )
+}
