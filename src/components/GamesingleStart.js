0d2465083ce0cca2f999e1857be46cc38260bbcf
Jason Yang <Jasonyang602@gmail.com>
2022-07-04 17:42:44 -0400

    Update GamesingleStart.js

--- src/components/GamesingleStart.js
+++ src/components/GamesingleStart.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { CSSTransition } from 'react-transition-group';
 
 export default function GamesingleStart() {
 
@@ -12,12 +12,18 @@ export default function GamesingleStart() {
     return (
         <div className="game-single-start">
             <h1 className="game-title">
-                {">"} TIC_TAC_TOE <br/><span className="blue">SINGLEPLAYER</span>
+                {">"} TIC_TAC_TOE <br/><span className="blueNoTyping">SINGLEPLAYER</span>
             </h1>
             <div className="start-game">START GAME</div>
             <h3 className="rules" onClick={toggleRules}>Rules</h3>
             {showRules && <div className="rules-text">
+                <CSSTransition
+                    in={showRules}
+                    timeout={500}
+                    classNames="rulestrans"
+                    >
                 <p className="rules-p"><span className="bold">Rules:</span> In this game, you will take turns placing symbols on a 3 by 3 grid. When a player connects 3 of their symbols in any horizontal, vertical or diagonal line they will win the round. Play continuosly against the CPU until you lose or draw. Players with the highest winstreaks will be featured on the leaderboards! GLHF!</p>
+                </CSSTransition >
             </div>}
         </div>
     )
