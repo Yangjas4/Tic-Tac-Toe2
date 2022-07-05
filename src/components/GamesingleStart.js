61f2abf9f056f472208942f0098b094e2345a3f6
Jason Yang <Jasonyang602@gmail.com>
2022-07-04 22:18:53 -0400

    animate rules

--- src/components/GamesingleStart.js
+++ src/components/GamesingleStart.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function GamesingleStart() {
 
@@ -15,9 +16,22 @@ export default function GamesingleStart() {
             </h1>
             <div className="start-game">START GAME</div>
             <h3 className="rules" onClick={toggleRules}>Rules</h3>
-            {showRules && <div className="rules-text">
-                <p className="rules-p"><span className="bold">Rules:</span> In this game, you will take turns placing symbols on a 3 by 3 grid. When a player connects 3 of their symbols in any horizontal, vertical or diagonal line they will win the round. Play continuosly against the CPU until you lose or draw. Players with the highest winstreaks will be featured on the leaderboards! GLHF!</p>
-            </div>}
+            <AnimatePresence>
+                {showRules && 
+                <motion.div 
+                    exit={{opacity: 0}}
+                    className="rules-text">
+                    <motion.p 
+                        initial={{opacity: 0}}
+                        animate={{ opacity: 1}}
+                        whileHover={{scale: 1.3}}
+                        className="rules-p"><span className="bold">Rules:</span> In this game, you
+                    will take turns placing symbols on a 3 by 3 grid. When a player connects 3 of their 
+                    symbols in any horizontal, vertical or diagonal line they will win the round. 
+                    Play continuosly against the CPU until you lose or draw. Players with the highest 
+                    winstreaks will be featured on the leaderboards! GLHF!</motion.p>
+                 </motion.div>}
+            </AnimatePresence>
         </div>
     )
 }
