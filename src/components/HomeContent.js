c408a253fe7c584f744bc5ec3120cef46856cca2
Jason Yang <Jasonyang602@gmail.com>
2022-07-07 16:48:23 -0400

    Update HomeContent.js

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -6,9 +6,9 @@ export default function HomeContent() {
         <main>
             <div className="cta">
                 <h1> {">"} WELCOME TO<br /><span className="blue">TIC_TAC_TOE</span></h1>
-                <p>A fork of my intro to computer science project made for the EECS 1012 course at York University which was also 
+                <p>A fork of my intro to computer science project made for the EECS 1012 course at York University which was also
                     the first project I'd ever made. I decided to rebuild as well as redesign
-                    the entire project using a react frontend and firebase backend. To the right you can find the original project's video demo 
+                    the entire project using a react frontend and firebase backend. To the right you can find the original project's video demo
                     as a point of comparison. You can alsp find the open source code on github at
                     <br /><a href="https://github.com/Yangjas4/Tic-Tac-Toe2">{" "}https://github.com/Yangjas4/Tic-Tac-Toe2.</a></p>
                 <div className="play-container">
