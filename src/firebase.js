86658f772c9d20e69f3dc64dc2f02b2f3d09d63a
Jason Yang <Jasonyang602@gmail.com>
2022-07-19 13:51:38 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from './firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAjtnXWdpUg1IRmFgkvCMmfehdZGd8wQRs",
     authDomain: "tictactoe-80221.firebaseapp.com",
@@ -8,8 +10,6 @@ const firebaseConfig = {
     appId: "1:450074633650:web:1b9cd0cfef6400d37db0ab",
     measurementId: "G-EDTQS7E86B"
   };
-
-  import firebase from 'firebase';
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
