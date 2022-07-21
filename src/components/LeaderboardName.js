c80c274feb59681308e0be9878291f6f2852774d
Jason Yang <Jasonyang602@gmail.com>
2022-07-20 23:55:40 -0400

    connected leaderboard to firestore

--- src/components/LeaderboardName.js
+++ src/components/LeaderboardName.js
@@ -1,2 +1,27 @@
+
 import React from 'react';
-import colRef from '../firebase';
+
+export default function LeaderboardName(props) {
+
+    const topThree = checkTopThree()
+
+    function checkTopThree() {
+        if (props.rank === 1) {
+            return "first-place"
+        } else if (props.rank === 2) {
+            return "second-place"
+        } else if (props.rank === 3) {
+            return "third-place"
+        } else {
+            return ""
+        }
+    }
+
+    return (
+        <tr>
+            <td className={`rank ${topThree}`}>#{props.rank}</td>
+            <td>{props.name}</td>
+            <td>{props.score}</td>
+        </tr>
+    )
+}
--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import LeaderboardName from '../components/LeaderboardName';
+import colRef from '../firebase';
+import { getDocs } from 'firebase/firestore';
+import { motion } from 'framer-motion';
 
 export default function Leaderboard() {
+
+    let leaderboardData;
+    const [leaderboardElements, setLeaderboardElements] = useState([]);
+    const [finishedLoading, setFinishedLoading] = useState(false);
+
+    useEffect(() => {
+        getDocs(colRef)
+            .then(snapshot => {
+                leaderboardData = snapshot.docs.map(doc => {
+                    return {
+                        ...doc.data()
+                    }
+                })
+                const sortedLeaderboardData = leaderboardData.sort((a, b) => b.winstreak - a.winstreak);
+                setLeaderboardElements(sortedLeaderboardData.map((player, index) => <LeaderboardName key={index} name={player.name} score={player.winstreak} rank={index + 1} />))
+                setFinishedLoading(true);
+            })
+            .catch(err => {
+                console.log(`%cError: ${err.message}`, "color:red");
+            })
+    }, [])
+
+
+
     return (
         <div className="app">
             <div className="margin">
                 <Navbar page="leaderboards" />
-                <div className="leaderboard-main">
-                    <h1 className="congrats">CONGRATS</h1>
-                    <div className="leaderboards-second-row">
-                        <h2 className="congrats-subheading">TO OUR TOP PLAYERS</h2>
-                        <div className="leaderboard-body">
-                            <h3 className="leaderboard-title">Winstreak High Scores</h3>
+                {finishedLoading ?
+                    <motion.div initial={{opacity: 0}} animate={{opacity: 1}}className="leaderboard-container">
+                        <div className="leaderboard-main">
+                            <h1 className="congrats">CONGRATS</h1>
+                            <div className="leaderboards-second-row">
+                                <h2 className="congrats-subheading">TO OUR TOP PLAYERS</h2>
+                                <div className="leaderboard-body">
+                                    <h3 className="leaderboard-title">Winstreak High Scores</h3>
+                                    <table className='leaderboard-table'>
+                                        <thead>
+                                            <tr>
+                                                <th>Rank</th>
+                                                <th>Name</th>
+                                                <th>Score</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {leaderboardElements}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    
-                </div>
+                    </motion.div> :
+                    <></>
+                }
             </div>
             <Footer />
         </div>
